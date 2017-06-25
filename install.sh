abd16a598a295670f43dd985d926b26e79355bde
Rin Tarama <km.uec80@gmail.com>
2017-06-26 00:49:34 +0900

    install.shが間違ってたのを修正

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 for f in .??*
 do
 	if [ "$f" = ".git" ]; then continue; fi
-	if [ "$f" == ".DS_Store" ]; then continue; fi
+	if [ "$f" = ".DS_Store" ]; then continue; fi
 	
   echo "$f"
 	ln -sf ~/dotfiles/$f ~/$f
