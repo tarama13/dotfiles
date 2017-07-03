7f122508b0e6bf3310d69c78c25d4ca4e18963cf
Rin Tarama <km.uec80@gmail.com>
2017-07-03 12:41:22 +0900

    デフォルトファイルの読み込み方の変更

--- uecwireless.sh
+++ uecwireless.sh
@@ -1,19 +1,24 @@
 #!/bin/bash
 proxy_server="proxy.uec.ac.jp"
 proxy_port="8080"
+settingfile="$HOME/dotfiles/gitconfig_proxy"
+
 if [ `iwgetid -r` = "UECWireless" ]; then
 	export http_proxy="http://$proxy_server:$proxy_port/"
 	export https_proxy="https://$proxy_server:$proxy_port/"
 	export ftp_proxy="http://$proxy_server:$proxy_port/"
+
+	# Git
 	echo -e "\
 [http]\n\
 	proxy = http://$proxy_server:$proxy_port/\n\
 [https]\n\
 	proxy = https://$proxy_server:$proxy_port/\n\
 [url \"https://\"]\n\
-	insteadOf = git://" > ~/.gitconfig_proxy
+	insteadOf = git://" > $settingfile
+
 else
 	unset http_proxy
 	unset https_proxy
-	echo "" > .gitconfig_proxy
+	rm $settingfile
 fi
