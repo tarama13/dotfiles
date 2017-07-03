c31c325d5b77d6c00612a252ea964e000b255f0a
Rin Tarama <km.uec80@gmail.com>
2017-07-03 18:12:02 +0900

    UECWirelessじゃないときに警告が出ないようにした

--- uecwireless.sh
+++ uecwireless.sh
@@ -3,7 +3,7 @@ proxy_server="proxy.uec.ac.jp"
 proxy_port="8080"
 settingfile="$HOME/dotfiles/gitconfig_proxy"
 
-if [ `iwgetid -r` = "UECWireless" ]; then
+if [[ `iwgetid -r` = "UECWireless" ]]; then
 	export http_proxy="http://$proxy_server:$proxy_port/"
 	export https_proxy="https://$proxy_server:$proxy_port/"
 	export ftp_proxy="http://$proxy_server:$proxy_port/"
@@ -20,5 +20,5 @@ if [ `iwgetid -r` = "UECWireless" ]; then
 else
 	unset http_proxy
 	unset https_proxy
-	rm $settingfile
+	echo "" > $settingfile
 fi
